352ef29b2775c35867e9498140bb963c8c8ef2e7
ymentawy <mentawy@aucegypt.edu>
2025-02-21 03:13:30 +0200

    added splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -6,10 +6,9 @@ import 'login_page.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
-    options:
-        DefaultFirebaseOptions.currentPlatform, // Use the generated options
+    options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(HomeSecurityApp());
+  runApp(const HomeSecurityApp());
 }
 
 class HomeSecurityApp extends StatefulWidget {
@@ -34,7 +33,75 @@ class HomeSecurityAppState extends State<HomeSecurityApp> {
       debugShowCheckedModeBanner: false,
       title: 'Home Security System',
       theme: isDarkMode ? ThemeData.dark() : ThemeData.light(),
-      home: LoginPage(),
+      home: const SplashScreen(), // Start with SplashScreen
+    );
+  }
+}
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+  late Animation<double> _fadeAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 800),
+    )..forward();
+
+    _fadeAnimation = CurvedAnimation(
+      parent: _animationController,
+      curve: Curves.easeInOut,
+    );
+
+    Future.delayed(const Duration(milliseconds: 2200), () {
+      Navigator.of(context).pushReplacement(
+        PageRouteBuilder(
+          pageBuilder: (_, __, ___) => const LoginPage(),
+          transitionsBuilder: (_, animation, __, child) {
+            return FadeTransition(
+              opacity: animation,
+              child: child,
+            );
+          },
+          transitionDuration: const Duration(milliseconds: 600),
+        ),
+      );
+    });
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor:
+          const Color(0xFF0B0730), // Matches the background color of the image
+      body: FadeTransition(
+        opacity: _fadeAnimation,
+        child: Center(
+          child: Image.asset(
+            'assets/splash_image.png',
+            fit: BoxFit
+                .contain, // Ensures the full image is visible without cropping
+            width: double.infinity,
+            height: double.infinity,
+          ),
+        ),
+      ),
     );
   }
 }
